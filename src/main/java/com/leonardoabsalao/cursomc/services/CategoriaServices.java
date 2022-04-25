67907d7f0c1ee58ab0be3ea729e7f2b09616b43f
Leo Absalao <leonardoabsalao@hotmail.com>
2022-04-25 00:28:01 -0300

    Criando respository e service para Categoria

---
+++ src/main/java/com/leonardoabsalao/cursomc/services/CategoriaServices.java
@@ -0,0 +1,22 @@
+package com.leonardoabsalao.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.leonardoabsalao.cursomc.domain.Categoria;
+import com.leonardoabsalao.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaServices {
+
+    @Autowired
+	private CategoriaRepository repo;
+    
+    //Fazendo uma busca no banco pelo seu Id.
+    public Categoria buscar(Integer id) {
+    	Optional<Categoria> obj = repo.findById(id);
+    	return obj.orElse(null);
+    	}
+    	
+}
